9acc79463cef57724ede0b04bfcdae874ec58ffc
jung35 <jung@jungoh.me>
2017-12-01 17:13:19 -0600

    win win win

--- src/script/data/hackathon.js
+++ src/script/data/hackathon.js
@@ -2,17 +2,17 @@ export default [
   {
     title: 'Subtle Scheme',
     timeline: 'Hack Illinois 2015',
-    description: 'Browser based game where players use phones as controllers to answer fill-in-the-blank trivia questions',
+    description: 'Browser based game where players use phones as controllers to answer fill-in-the-blank trivia questions. Won best networked game',
     tags: ['react', 'html-bootstrap', 'node-socket.io', 'node-express.js', 'mongodb']
   }, {
     title: 'MechMania',
     timeline: 'MechMania 2017',
-    description: 'AI to compete in a 3v3 combat arena',
+    description: 'AI to compete in a 3v3 combat arena. Ended top 10',
     tags: ['csharp-C#', 'csharp-Unreal']
   }, {
     title: 'Electronic Trading Challenge',
     timeline: 'Jane Street ETC 2017',
-    description: 'AI to compete in a simulated market trading challenge',
+    description: 'AI to compete in a simulated market trading challenge. Won 1st place',
     tags: ['python']
   },
 ]
