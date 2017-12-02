54d2b98e601407e23ecd20672cf0d05e691b20d3
jung35 <jung@jungoh.me>
2017-12-01 18:29:14 -0600

    more words

--- src/script/data/projects.js
+++ src/script/data/projects.js
@@ -2,12 +2,12 @@ export default [
   {
     title: 'VacStatus',
     timeline: '2014 - 2016',
-    description: 'Web application to track cheaters for games that use Valve Anti-Cheat (VAC)',
+    description: 'Web application that tracks cheaters for games that use the Valve Anti-Cheat (VAC)',
     tags: ['php-laravel', 'html-bootstrap', 'react']
   }, {
     title: '10 Man Bot',
     timeline: '2017',
-    description: 'Discord bot to help setup teams and map votes for private counter-strike match',
+    description: 'Discord bot created to help setup teams and map votes for private counter-strike match',
     tags: ['node']
   },
 ]
