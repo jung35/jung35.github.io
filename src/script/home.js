03d7a060b2a6d98829a592647efff30b8357105e
jung35 <jung@jungoh.me>
2017-12-03 18:44:41 -0600

    logos

--- src/script/home.js
+++ src/script/home.js
@@ -15,61 +15,63 @@ export default class Home extends Component {
       <div className='home-projects'>
         <h2>Projects</h2>
         <ul className='list'>
-          {
-            projectData.map(project => {
-              return <li key={ project.title }>
-                <div className="list-header">
-                  <div className="title">{ project.title }</div>
-                  <div className="timeline">{ project.timeline }</div>
-                </div>
-                <p>{ project.description }</p>
-                <ul className="tags">
-                  {
-                    project.tags.map(tag => {
-                      const splitTag = tag.split('-')
-                      if(splitTag.length == 1) {
-                        return <li key={ splitTag[0] } className={ splitTag[0] }>{ splitTag[0] }</li>
-                      }
+          {projectData.map(project => {
+            return <li key={ project.title }>
+              <div className="list-header">
+                <div className="title">{ project.title }</div>
+                <div className="timeline">{ project.timeline }</div>
+              </div>
+              <p>{ project.description }</p>
+              <ul className="tags">
+                {
+                  project.tags.map(tag => {
+                    const splitTag = tag.split('-')
+                    if(splitTag.length == 1) {
+                      return <li key={ splitTag[0] } className={ splitTag[0] }>{ splitTag[0] }</li>
+                    }
 
-                      return <li key={ splitTag[1] } className={ splitTag[0] }>{ splitTag[1] }</li>
-                    })
-                  }
-                </ul>
-              </li>
-            })
-          }
+                    return <li key={ splitTag[1] } className={ splitTag[0] }>{ splitTag[1] }</li>
+                  })
+                }
+              </ul>
+            </li>
+          })}
         </ul>
       </div>
       <div className="home-work">
-        <h2>Works</h2>
-        <p>...WIP</p>
+        <h2>Clients</h2>
+        <ul className="companies">
+          {workData.map(work => {
+            return <li key={ work.title }>
+              <img src={ work.logo } alt={ `${work.title} Logo` }/>
+            </li>
+          })}
+        </ul>
       </div>
       <div className="home-hackathon">
         <h2>Hackathon <small>&mdash; Coding competition held for 12 to 48 hours to create a software. (<a href="https://en.wikipedia.org/wiki/Hackathon" target="_blank">wikipedia</a>)</small></h2>
         <ul className='list'>
-          {
-            hackathonData.map(hackathon => {
-              return <li key={ hackathon.title }>
-                <div className="list-header">
-                  <div className="title">{ hackathon.title }</div>
-                  <div className="timeline">{ hackathon.timeline }</div>
-                </div>
-                <p>{ hackathon.description }</p>
-                <ul className="tags">
-                  {
-                    hackathon.tags.map(tag => {
-                      const splitTag = tag.split('-')
-                      if(splitTag.length == 1) {
-                        return <li key={ splitTag[0] } className={ splitTag[0] }>{ splitTag[0] }</li>
-                      }
+          {hackathonData.map(hackathon => {
+            return <li key={ hackathon.title }>
+              <div className="list-header">
+                <div className="title">{ hackathon.title }</div>
+                <div className="timeline">{ hackathon.timeline }</div>
+              </div>
+              <p>{ hackathon.description }</p>
+              <ul className="tags">
+                {
+                  hackathon.tags.map(tag => {
+                    const splitTag = tag.split('-')
+                    if(splitTag.length == 1) {
+                      return <li key={ splitTag[0] } className={ splitTag[0] }>{ splitTag[0] }</li>
+                    }
 
-                      return <li key={ splitTag[1] } className={ splitTag[0] }>{ splitTag[1] }</li>
-                    })
-                  }
-                </ul>
-              </li>
-            })
-          }
+                    return <li key={ splitTag[1] } className={ splitTag[0] }>{ splitTag[1] }</li>
+                  })
+                }
+              </ul>
+            </li>
+          })}
         </ul>
       </div>
     </div>
